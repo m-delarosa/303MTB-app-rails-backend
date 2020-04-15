6322a8c2f998705011b992ee0a81a1df4fb3a546
Michael De La Rosa <56710845+m-delarosa@users.noreply.github.com>
2020-04-14 23:01:45 -0600

    Add user model w/ validations, routes, and action methods.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+        render json: @users
+    end
+        
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+        
+    def create
+        @user = User.new(user_params)
+        if @user.save
+        render json: {message: "User was successfully created!", user: @user}
+        else
+        render json: @user.errors.messages
+        end
+    end
+        
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+        render json: @user
+        else
+        render json: @user.errors.messages
+        end
+    end
+        
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        render json: {message: "User account sucessfully deleted"} 
+    end
+        
+    private
+    
+    def user_params
+        params.require(:user).permit([:name, :username, :email, :password])
+    end
+end
