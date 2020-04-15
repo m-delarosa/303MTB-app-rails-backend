daede2add62d48f5c034905ca91153973685392e
Michael De La Rosa <56710845+m-delarosa@users.noreply.github.com>
2020-04-14 23:27:39 -0600

    Add user token decoding and authentication to user index, show, update and destroy actions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+    def authenticate
+        begin
+            authorization_header = request.headers["Authorization"]
+            token = authorization_header.split(" ")[1]
+            secret = Rails.application.secrets.secret_key_base
+            decoded_token = JWT.decode(token, secret)
+        rescue
+            render json: { message: "Invalid token! Try logging in again." }
+        end
+    end
 end
