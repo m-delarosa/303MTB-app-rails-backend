6c38985ee3a89e6ea8f066617212b352f8288650
github username <56710845+m-delarosa@users.noreply.github.com>
2020-10-22 21:47:36 -0600

    Update routes to exclude comments.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   resources :trails, only: [:index, :show, :update]
   resources :users, only: [:index, :show, :create, :update, :destroy]
   post "/login", to: "authentication#login"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
