f5ebe8181daa1d19320057b1dde0d0da67c9f6da
github username <56710845+m-delarosa@users.noreply.github.com>
2020-10-23 20:30:07 -0600

    Add if/else test and token, encryption log.

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,12 +5,20 @@ class AuthenticationController < ApplicationController
         if @user
             if @user.authenticate(params[:password])
                 payload = { user_id: @user.id }
-                puts "before secret"
+                puts "before secret marker"
                 secret = Rails.application.secrets.secret_key_base
-                puts "after secret"
+                puts "after secret marker"
                 token = JWT.encode(payload, secret)
+                puts "after token encode marker"
+                puts "secret is" secret
+                puts "token is" token
 
-                render json: { token: token, message: "Account login was sucessful!", user: @user }
+                if token 
+                    render json: { token: token, message: "Account login was sucessful!", user: @user }
+                else
+                    render json: { message: "No Token"}
+                end
+                
             else
             render json: { message: "Please verify login credentials and try again."}
             end
